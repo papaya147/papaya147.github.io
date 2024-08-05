98ef7d5fb3d1e81a0874a6294db560ba1b8fd7e3
Abhinav Srivatsa <asrivatsa6@gmail.com>
2024-08-05 10:34:02 +0530

    start wynters dreams content

--- projects/project-content.js
+++ projects/project-content.js
@@ -8,40 +8,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
   headings.forEach(heading => {
     if (heading.tagName === "H1") {
-      // Assign an ID to the h1 element
       heading.id = `${h1Counter}`
 
-      // Create an anchor element
       const link = document.createElement("a")
 
-      // Set the href attribute to the id of the heading
       link.href = `#${heading.id}`
       link.className = "section-heading"
       link.textContent = heading.textContent
 
-      // Append the link to the sidebar
       sidebar.appendChild(link)
 
-      // Increment the h1 counter and reset the h2 counter
       h1Counter++
       h2Counter = 1
 
     } else if (heading.tagName === "H2") {
-      // Assign an ID to the h2 element
       heading.id = `${h1Counter - 1}-${h2Counter}`
 
-      // Create an anchor element
       const link = document.createElement("a")
 
-      // Set the href attribute to the id of the heading
       link.href = `#${heading.id}`
       link.className = "section-subheading"
       link.textContent = heading.textContent
 
-      // Append the link to the sidebar
       sidebar.appendChild(link)
 
-      // Increment the h2 counter
       h2Counter++
     }
   })
